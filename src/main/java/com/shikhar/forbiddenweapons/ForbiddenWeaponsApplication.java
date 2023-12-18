5877acf2dc0ff898396613d7aed9c2ebbadc8f43
Shikhar <shikhar1337@gmail.com>
2023-12-18 16:38:44 +0530

    Initializing Repo

---
+++ src/main/java/com/shikhar/forbiddenweapons/ForbiddenWeaponsApplication.java
@@ -0,0 +1,13 @@
+package com.shikhar.forbiddenweapons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForbiddenWeaponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForbiddenWeaponsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shikhar/forbiddenweapons/ForbiddenWeaponsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikhar.forbiddenweapons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForbiddenWeaponsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
